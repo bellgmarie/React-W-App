e5d262293e79f56202c912c84e6de183c52824db
bell <108286428+bellgmarie@users.noreply.github.com>
2023-03-26 22:25:23 -0400

    added unit conversion and unitconvert component

--- src/UnitConvert.js
+++ src/UnitConvert.js
@@ -2,18 +2,44 @@ import React, { useState } from "react";
 
 export default function UnitConvert(props) {
   const [unit, setUnit] = useState("imperial");
+  function convertCelsius(event) {
+    event.preventDefault();
+    setUnit("convertFahrenheit");
+  }
+  function convertFarenheit(event) {
+    event.preventDefault();
+    setUnit("imperial");
+  }
+
   if (unit === "imperial") {
     return (
       <div>
         <span className="col-6 temp "> {props.farenheit}°</span>
         <span className="col-8 degreeChange ">
-          <span className="unit">
-            F | <a href="/">C</a>{" "}
+          <span className="Unit">
+            F |{" "}
+            <a href="/" onClick={convertCelsius}>
+              C
+            </a>{" "}
           </span>
         </span>
       </div>
     );
   } else {
-    return "F";
+    let celsiusTemperature = ((props.farenheit - 32) * 5) / 9;
+
+    return (
+      <div>
+        <span className="col-6 temp "> {Math.round(celsiusTemperature)}°</span>
+        <span className="col-8 degreeChange ">
+          <span className="Unit">
+            <a href="/" onClick={convertFarenheit}>
+              F
+            </a>{" "}
+            | C
+          </span>
+        </span>
+      </div>
+    );
   }
 }
